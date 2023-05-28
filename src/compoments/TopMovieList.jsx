71434167f5e7ddfec51324a417aba4fee725d538
FizKw <hafizhkumara.w@gmail.com>
2023-05-28 14:31:54 +0700

    dareil pinal

--- src/compoments/TopMovieList.jsx
+++ src/compoments/TopMovieList.jsx
@@ -12,6 +12,9 @@ function TopMovieList() {
   const [idGenre, setIdGenre] = React.useState();
   const [isMax, setIsMax] = React.useState(false);
   const [visible, setVisible] = React.useState(false);
+  let detailGenre = [];
+
+
 
   React.useEffect(() => {
     fetchTopList(1).then((result) => {
