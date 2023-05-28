7189e609b045b0a5526959fa2d35929fe8a6993d
FizKw <hafizhkumara.w@gmail.com>
2023-05-28 18:00:54 +0700

    ktnya ma final

--- src/compoments/TopMovieList.jsx
+++ src/compoments/TopMovieList.jsx
@@ -13,8 +13,6 @@ function TopMovieList() {
   const [isMax, setIsMax] = React.useState(false);
   const [visible, setVisible] = React.useState(false);
 
-
-
   React.useEffect(() => {
     fetchTopList(1).then((result) => {
       setTopMovies(result);
@@ -57,38 +55,39 @@ function TopMovieList() {
   const Movies = () => {
     return topMovies.map((movie, index) => {
       return (
-        <div className="MovieWrapper p-2 w-full " key={movie.id}>
-          <div className="card bg-base-300 shadow-xl p-2 flex items-center sm:flex-row ">
-            <Link to={`/details/${movie.id}`}>
+        <div className="MovieWrapper group p-2 w-full " key={movie.id}>
+          <Link to={`/details/${movie.id}`}>
+            <div className="card bg-base-300 shadow-xl group-hover:bg-secondary p-2 flex items-center sm:flex-row ">
               <img
                 src={`${process.env.REACT_APP_BASEIMGURL}/${movie.poster_path}`}
                 alt="img not found"
-                className="moviePoster rounded-lg m-2"
+                className="moviePoster rounded-lg m-2 filter group-hover:brightness-75"
               />
-            </Link>
-            <div className="self-center flex sm:flex-row sm:w-full p-3">
-              <div className="basis-10/12">
-                <Link to={`/details/${movie.id}`}>
-                  <h1 className="text-lg">
+
+              <div className="self-center flex sm:flex-row sm:w-full p-3">
+                <div className="basis-10/12">
+                  <h1 className="text-lg group-hover:text-white">
                     {index + 1}. {movie.title} ({movie.release_date})
                   </h1>
-                </Link>
-              </div>
-              <div className="flex align-middle text-center">
-                <svg
-                  aria-hidden="true"
-                  className="w-5 h-5 text-yellow-400 pt-1"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <title>Rating star</title>
-                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-                </svg>
-                <p className="basis-2/12">{movie.vote_average}</p>
+                </div>
+                <div className="flex align-middle text-center">
+                  <svg
+                    aria-hidden="true"
+                    className="w-5 h-5 text-yellow-400 pt-1"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <title>Rating star</title>
+                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                  </svg>
+                  <p className="basis-2/12 group-hover:text-white">
+                    {movie.vote_average}
+                  </p>
+                </div>
               </div>
             </div>
-          </div>
+          </Link>
         </div>
       );
     });
@@ -151,7 +150,7 @@ function TopMovieList() {
         <Movies />
         <button
           type="button"
-          className={`btn m-6 btn-xs sm:btn-sm md:btn-md lg:btn-lg btn-ghost ${
+          className={`btn m-6 btn-xs sm:btn-sm md:btn-md  lg:btn-lg btn-ghost ${
             isMax ? "hidden" : ""
           }`}
           onClick={() => {
